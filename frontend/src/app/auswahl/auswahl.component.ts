07aff8371e2bb98151b9ea14f987d7e975b5e2ce
Oster Valery <oster.valera@hotmail.com>
2021-05-17 15:45:40 +0200

    auswahl.compoment, linklogo and addlogo

---
+++ frontend/src/app/auswahl/auswahl.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-auswahl',
+  templateUrl: './auswahl.component.html',
+  styleUrls: ['./auswahl.component.css']
+})
+export class AuswahlComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
