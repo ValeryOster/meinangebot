59b2b3d0f183455e56e947c66bc2609e434711fc
Oster Valery <oster.valera@hotmail.com>
2021-06-05 19:39:21 +0200

    https withdraw

--- frontend/src/app/service/security/auth.service.ts
+++ frontend/src/app/service/security/auth.service.ts
@@ -17,7 +17,6 @@ export class AuthService {
   constructor(private http: HttpClient) { }
 
   login(credentials): Observable<any> {
-    console.log("ashedf");
     return this.http.post(AUTH_API + 'signin', {
       username: credentials.username,
       password: credentials.password
