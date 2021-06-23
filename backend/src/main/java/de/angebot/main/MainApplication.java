f1daaaed40cdfe1d21a660918a2a438e210b379e
Oster Valery <oster.valera@hotmail.com>
2021-06-23 14:25:55 +0200

    test 2 is done

--- backend/src/main/java/de/angebot/main/MainApplication.java
+++ backend/src/main/java/de/angebot/main/MainApplication.java
@@ -4,6 +4,7 @@ import org.apache.catalina.Context;
 import org.apache.catalina.connector.Connector;
 import org.apache.tomcat.util.descriptor.web.SecurityCollection;
 import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.apache.tomcat.util.net.TLSClientHelloExtractor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -11,11 +12,15 @@ import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactor
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 
+import java.nio.charset.StandardCharsets;
+
+
 @SpringBootApplication
 @EnableConfigurationProperties
 public class MainApplication {
 
     public static void main(String[] args) {
+        setRedirectionHttpToHttps();
         SpringApplication.run(MainApplication.class, args);
     }
 
@@ -38,10 +43,27 @@ public class MainApplication {
 
     private Connector redirectConnector() {
         Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-        connector.setScheme("http");
-        connector.setPort(8080);
+        connector.setScheme("https");
+        connector.setPort(80);
         connector.setSecure(false);
-        connector.setRedirectPort(80);
+        connector.setRedirectPort(443);
         return connector;
+    }
+
+    private static void setRedirectionHttpToHttps() {
+        TLSClientHelloExtractor.USE_TLS_RESPONSE = ("<html>\n"+
+                "<head>\n" +
+                "<title> Redicting to HTTPS</title> <meta http-equiv='refresh' content=\"0; " +
+                "url=https://localhost:443/\" />" +
+                "</head>\n" +
+                "<script Language=JavaScript>\n" +
+                "Function redirectHttpToHttps()\n" +
+                "{" +
+                "window.location = 'https://localhost:443/';\n" +
+                "}" +
+                "redirectHttpToHttps();" +
+                "</script>" +
+                "</body>" +
+                "</html> ").getBytes(StandardCharsets.UTF_8);
     }
 }
