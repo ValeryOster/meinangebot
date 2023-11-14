b6fa3974c3c72dce64a71a798cdadf754f6cca94
Valery Oster <v.oster@optadata-gruppe.de>
2023-11-14 13:03:42 +0100

    ErrorMessenger with Logger

--- backend/src/main/java/de/angebot/main/errors/ErrorMessenger.java
+++ backend/src/main/java/de/angebot/main/errors/ErrorMessenger.java
@@ -1,8 +1,12 @@
 package de.angebot.main.errors;
 
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class ErrorMessenger {
 
     public void send(String message) {
-        System.out.println(message);
+        log.error(message);
     }
 }
